2ceb0df149d944192cf43b4c43bdbc4ea6ba79c8
Kamil Gorzynski <kamil.gorzynski8@gmail.com>
2021-01-18 20:18:08 +0100

    Modify webpage script update

--- modify_webpage.py
+++ modify_webpage.py
@@ -1,10 +1,11 @@
 from selenium import webdriver, common
 from selenium.webdriver.common.keys import Keys
 import time
+from random import uniform
 import config
 
 driver = webdriver.Firefox()
-driver.get("https://twitter.com/")
+driver.get("https://twitter.com/login")
 
 
 time.sleep(4)
@@ -22,9 +23,27 @@ password.clear()
 email.send_keys(config.ACCOUNT)
 password.send_keys(config.PASSWD)
 password.send_keys(Keys.RETURN)
-time.sleep(4)
-tweet=driver.find_elements_by_xpath("//*[contains(text(), 'unique_tweet_to_color')]")
-print(tweet)
-#driver.execute_script("arguments[0].style.color = 'lightblue'", tweet[0])
-driver.execute_script("arguments[0].style.color = 'blue'", tweet[0])
-print(tweet)
+time.sleep(uniform(4, 8))
+
+# div:
+# css-901oao r-jwli3a r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0
+
+# span:
+# css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0
+
+# span2:
+# /html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[4]/div/div/section/div/div/div[4]/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span
+# css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0
+
+# div:
+# css-901oao r-jwli3a r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0
+# css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0
+
+xpath = "/html/body/div/div/div/div/main/div/div/div/div/div/div/div/div/section/div/div/div/div/div/article/div/div/div/div/div/div/div/div/span"
+#tweets=driver.find_elements_by_xpath(xpath)
+tweets = driver.find_elements_by_css_selector("span .css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0")
+texts = [tweet.text for tweet in tweets]
+print(tweets[0])
+#driver.execute_script("arguments[0].style.color = 'lightblue'", tweets[0])
+driver.execute_script("arguments[0].style.color = 'blue'", tweets[0])
+print(tweets[0])
