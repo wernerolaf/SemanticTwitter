25b7f0e49b5693174b51e9009830e382cc809a19
Kamil Gorzynski <kamil.gorzynski8@gmail.com>
2021-01-19 19:24:35 +0100

    Find tweets by css selector

--- modify_webpage.py
+++ modify_webpage.py
@@ -7,6 +7,9 @@ import config
 
 driver = webdriver.Firefox()
 
+def sleep():
+    time.sleep(uniform(4, 8))
+
 def revisit():
     driver.get("https://twitter.com")
     cookies = pickle.load(open("cookies.pkl", "rb"))
@@ -15,13 +18,13 @@ def revisit():
 
 def visit():
     driver.get("https://twitter.com/login")
-    time.sleep(4)
+    sleep()
 
     try:
         email = driver.find_element_by_name('session[username_or_email]')
         password = driver.find_element_by_name('session[password]')
     except common.exceptions.NoSuchElementException:
-        time.sleep(3)
+        sleep()
         email = driver.find_element_by_name('session[username_or_email]')
         password = driver.find_element_by_name('session[password]')
 
@@ -30,13 +33,14 @@ def visit():
     email.send_keys(config.ACCOUNT)
     password.send_keys(config.PASSWD)
     password.send_keys(Keys.RETURN)
-    time.sleep(uniform(4, 8))
+    sleep()
 
     # dump cookies
     pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
 
 # visit()
 revisit()
+sleep()
 
 # div:
 # css-901oao r-jwli3a r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0
@@ -52,11 +56,16 @@ revisit()
 # css-901oao r-jwli3a r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0
 # css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0
 
-xpath = "/html/body/div/div/div/div/main/div/div/div/div/div/div/div/div/section/div/div/div/div/div/article/div/div/div/div/div/div/div/div/span"
-#tweets=driver.find_elements_by_xpath(xpath)
-tweets = driver.find_elements_by_css_selector("span .css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0")
+# div:
+# css-901oao r-jwli3a r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0
+
+# xpath = "/html/body/div/div/div/div/main/div/div/div/div/div/div/div/div/section/div/div/div/div/div/article/div/div/div/div/div/div/div/div/span"
+# tweets1 = driver.find_elements_by_xpath(xpath)
+# tweets2 = driver.find_elements_by_css_selector("span .css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0")
+tweets = driver.find_elements_by_css_selector("div .css-901oao.r-jwli3a.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0")
+
 texts = [tweet.text for tweet in tweets]
-print(tweets[0])
+print(texts[0])
 #driver.execute_script("arguments[0].style.color = 'lightblue'", tweets[0])
 driver.execute_script("arguments[0].style.color = 'blue'", tweets[0])
-print(tweets[0])
+print(texts[0])
